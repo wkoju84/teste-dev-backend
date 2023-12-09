71fa9e3f174479529bf6c2fc559a97e21dab1c63
wkoju84 <wkoju84@gmail.com>
2023-12-09 20:14:58 -0300

    Add package structure and include swagger for documentation

---
+++ dev/src/main/java/br/william/testedevbackend/TesteDevBackendApplication.java
@@ -0,0 +1,13 @@
+package br.william.testedevbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDevBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDevBackendApplication.class, args);
+	}
+
+}
---
+++ dev/src/test/java/br/william/testedevbackend/TesteDevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.william.testedevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDevBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
