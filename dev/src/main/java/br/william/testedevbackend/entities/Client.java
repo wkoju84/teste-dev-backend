6dba414e3f6b2d354432183b340a3d7c57331e85
wkoju84 <wkoju84@gmail.com>
2023-12-18 18:26:03 -0300

    add ClientController and Client Repository class

---
+++ dev/src/main/java/br/william/testedevbackend/entities/Client.java
@@ -0,0 +1,91 @@
+package br.william.testedevbackend.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Date dateOfBirth;
+    private String gender;
+    private HealthProblem healthProblem;
+    private LocalDate creationDate;
+    private LocalDate updateDate;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, Date dateOfBirth, String gender, HealthProblem healthProblem,
+                  LocalDate creationDate, LocalDate updateDate) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.healthProblem = healthProblem;
+        this.creationDate = creationDate;
+        this.updateDate = updateDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public HealthProblem getHealthProblem() {
+        return healthProblem;
+    }
+
+    public void setHealthProblem(HealthProblem healthProblem) {
+        this.healthProblem = healthProblem;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDate getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDate updateDate) {
+        this.updateDate = updateDate;
+    }
+}
