f21a2f8f7672ac138102596ea311d5af23502709
wkoju84 <wkoju84@gmail.com>
2023-12-19 10:31:01 -0300

    add search customer by id in the ClientController layer

--- dev/src/main/java/br/william/testedevbackend/controllers/ClientController.java
+++ dev/src/main/java/br/william/testedevbackend/controllers/ClientController.java
@@ -5,6 +5,7 @@ import br.william.testedevbackend.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,11 @@ public class ClientController {
     public ResponseEntity<List<ClientDto>> searchAllCustomers(){
         List<ClientDto> list = clientService.searchAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDto> searchCustomerById(@PathVariable Long id){
+        ClientDto dto = clientService.searchById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
